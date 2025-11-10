674c1cb92b5572a4521247e144995c58eca3873c
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-11-10 13:20:12 -0800

    set it up using the workflow from lab

--- frontend/tests/Sample.test.ts
+++ frontend/tests/Sample.test.ts
@@ -2,13 +2,21 @@
 Source: https://medium.com/@dimi_2011/tdd-unit-testing-typescript-project-with-jest-2557e8b84414
 */
 import { IntroScreenModel } from "../src/Intro/screens/IntroScreenModel.ts";
+import { describe, it, expect } from "vitest";
 
-describe("Sample test suite", () => {
-  test("Initial test", () => {
-     expect(3).toEqual(3);
-  });
-  test("Intro Scene Model", () => {
-    let s = new IntroScreenModel();
-    expect(s.getState()).toEqual(0);  
+let s = new IntroScreenModel();
+// describe("Sample test suite", () => {
+//   test("Initial test", () => {
+//      expect(3).toEqual(3);
+//   });
+//   test("Intro Scene Model", () => {
+//     let s = new IntroScreenModel();
+//     expect(s.getState()).toEqual(0);  
+//   });
+// });
+describe("IntroSceneModel", () => {
+  it("Intro Scene Model state", () => {
+    expect(s.getState()).toEqual(0);
   });
 });
+
