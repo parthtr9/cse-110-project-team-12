229f4485e49dd248f97df181d497d7018925f53f
gavinlin24 <gavinlin24@gmail.com>
2025-11-12 09:49:40 -0800

    Add data for country flags + story scenarios

---
+++ frontend/src/FlagMinigame/data/Scenarios.ts
@@ -0,0 +1,12 @@
+export const Scenarios: string[] = [
+    "You are lost at the airport and need to find your transferring flight to",
+    "You need to call the embassy of",
+    "You're buying a souvenir from",
+    "You're looking for the gate to",
+    "You need to find the tourist information desk for",
+    "You're searching for the customs line for",
+    "You want to exchange currency for",
+    "You're looking for a visa application for",
+    "You need to find the baggage claim for",
+    "You're trying to book a hotel in"
+];
